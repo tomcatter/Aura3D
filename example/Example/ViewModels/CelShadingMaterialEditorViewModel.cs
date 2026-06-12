4021c401ddb67e7721b26bd9b43b5ff4f0d90e23
Sigmaphy <11286815+sigmaphy@user.noreply.gitee.com>
2026-06-12 11:39:44 +0800

    卡渲编辑器改用ColorPicker拾取颜色

--- example/Example/ViewModels/CelShadingMaterialEditorViewModel.cs
+++ example/Example/ViewModels/CelShadingMaterialEditorViewModel.cs
@@ -74,45 +74,11 @@ public class ParameterItem : ObservableObject
         {
             if (SetProperty(ref _colorValue, value))
             {
-                _syncingText = true;
-                RText = (value.R / 255f).ToString("F3");
-                GText = (value.G / 255f).ToString("F3");
-                BText = (value.B / 255f).ToString("F3");
-                AText = (value.A / 255f).ToString("F3");
                 StringValue = OriginalValue is Vector3
                     ? $"({value.R / 255f:F3}, {value.G / 255f:F3}, {value.B / 255f:F3})"
                     : $"({value.R / 255f:F3}, {value.G / 255f:F3}, {value.B / 255f:F3}, {value.A / 255f:F3})";
-                _syncingText = false;
             }
         }
-    }
-
-    private string _rText = "";
-    public string RText
-    {
-        get => _rText;
-        set => SetProperty(ref _rText, value);
-    }
-
-    private string _gText = "";
-    public string GText
-    {
-        get => _gText;
-        set => SetProperty(ref _gText, value);
-    }
-
-    private string _bText = "";
-    public string BText
-    {
-        get => _bText;
-        set => SetProperty(ref _bText, value);
-    }
-
-    private string _aText = "";
-    public string AText
-    {
-        get => _aText;
-        set => SetProperty(ref _aText, value);
     }
 
     public bool IsColor => Kind == ParameterKind.Color;
@@ -120,7 +86,6 @@ public class ParameterItem : ObservableObject
     public bool IsInt => Kind == ParameterKind.Int;
     public bool IsOther => Kind == ParameterKind.Other;
 
-    private bool _syncingText;
     private bool _applying;
 
     public ParameterItem(string key, object value, Material material)
@@ -139,10 +104,6 @@ public class ParameterItem : ObservableObject
                     (byte)(Math.Clamp(v4.X, 0, 1) * 255),
                     (byte)(Math.Clamp(v4.Y, 0, 1) * 255),
                     (byte)(Math.Clamp(v4.Z, 0, 1) * 255));
-                _rText = (v4.X).ToString("F3");
-                _gText = (v4.Y).ToString("F3");
-                _bText = (v4.Z).ToString("F3");
-                _aText = (v4.W).ToString("F3");
                 _stringValue = $"({v4.X:F3}, {v4.Y:F3}, {v4.Z:F3}, {v4.W:F3})";
                 break;
             case Vector3 v3:
@@ -151,10 +112,6 @@ public class ParameterItem : ObservableObject
                     (byte)(Math.Clamp(v3.X, 0, 1) * 255),
                     (byte)(Math.Clamp(v3.Y, 0, 1) * 255),
                     (byte)(Math.Clamp(v3.Z, 0, 1) * 255));
-                _rText = (v3.X).ToString("F3");
-                _gText = (v3.Y).ToString("F3");
-                _bText = (v3.Z).ToString("F3");
-                _aText = "1.000";
                 _stringValue = $"({v3.X:F3}, {v3.Y:F3}, {v3.Z:F3})";
                 break;
             case float f:
@@ -189,9 +146,6 @@ public class ParameterItem : ObservableObject
             case nameof(ColorValue):
                 ApplyColorToMaterial();
                 break;
-            case nameof(RText) or nameof(GText) or nameof(BText) or nameof(AText):
-                if (!_syncingText) ApplyTextToColor();
-                break;
             case nameof(StringValue):
                 if (Kind is ParameterKind.Float or ParameterKind.Int) ApplyScalarToMaterial();
                 break;
@@ -212,19 +166,6 @@ public class ParameterItem : ObservableObject
                 break;
         }
         _applying = false;
-    }
-
-    private void ApplyTextToColor()
-    {
-        if (float.TryParse(RText, out var r) && float.TryParse(GText, out var g) &&
-            float.TryParse(BText, out var b) && float.TryParse(AText, out var a))
-        {
-            ColorValue = Color.FromArgb(
-                (byte)(Math.Clamp(a, 0, 1) * 255),
-                (byte)(Math.Clamp(r, 0, 1) * 255),
-                (byte)(Math.Clamp(g, 0, 1) * 255),
-                (byte)(Math.Clamp(b, 0, 1) * 255));
-        }
     }
 
     private void ApplyScalarToMaterial()
